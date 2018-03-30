b5d44ea9410329b30562c6a017847d7f5f340fad
Ryan O'Toole <otoolerj@gmail.com>
2018-03-30 21:15:39 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,14 @@
-def funk
-  'yo cat'
-end 
+require 'pry'
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+# esperanto_array = ESPERANTO_ALPHABET.split("")
+
+def alphabetize(arr)
+# ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+esperanto_array = ESPERANTO_ALPHABET.split("")
+  arr.sort_by do |string|
+    string.split("").collect do |letter|
+      esperanto_array.index(letter)
+    end
+  end
+end
