b2fde087180b9fe08a62c9ff7f34ebf88fc3fe2b
Ryan O'Toole <otoolerj@gmail.com>
2018-03-30 21:15:05 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,14 +1,1 @@
-require 'pry'
-
-ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-# esperanto_array = ESPERANTO_ALPHABET.split("")
-
-def alphabetize(arr)
-# ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-esperanto_array = ESPERANTO_ALPHABET.split("")
-  arr.sort_by do |string|
-    string.split("").collect do |letter|
-      esperanto_array.index(letter)
-    end
-  end
-end
+funk your cat
